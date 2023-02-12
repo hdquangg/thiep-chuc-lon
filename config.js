b796913beefd8bc68e17c74777693fee7353f430
hdquangg <125178299+hdquangg@users.noreply.github.com>
2023-02-12 13:57:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc lợn mãi xinh 🐽';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
